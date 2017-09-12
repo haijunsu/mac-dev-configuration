133be286d2f5e5c786a3ce390fa0c3ffd3e67a65
Haijun Su <navysu@gmail.com>
2017-09-12 13:04:12 -0400

    remove brew update for some roles

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ set -e
 # if either of the conditions are met, install dev tools
 if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]]; then
     echo "Info   | Install   | xcode"
-    sudo code-select --install
+    sudo xcode-select --install
 fi
 
 # Download and install pip
